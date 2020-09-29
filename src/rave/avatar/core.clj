5b305d21bc2efd6aa902a394f577aa28aa327062
Jonathan Millett <jon@millett.net>
2020-09-29 18:02:47 -0300

    Got working: PNG and JPG resizing, list images, serve image, gallery page.

--- src/rave/avatar/core.clj
+++ src/rave/avatar/core.clj
@@ -5,26 +5,40 @@
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [GET POST defroutes]]
             [compojure.route :refer [not-found]]
+            [mikera.image.core :as img]
+            [clojure.java.io :refer [input-stream]]
             [clojure.data.json :as json]
             [taoensso.timbre :refer [info debug]]
-            [clojure.spec.alpha :as s]
-            [fipp.edn :refer [pprint]]))
+            [clojure.string :as str]
+            [hiccup.core :refer [html]]
+            [hiccup.element :as h]
+            [fipp.edn :refer [pprint]])
+  (:import [java.awt.image BufferedImage]
+           [java.awt Color]
+           [org.imgscalr Scalr]))
+
+;; References
+;; - http://javadox.com/org.imgscalr/imgscalr-lib/4.2/org/imgscalr/Scalr.html
 
 ;; ----- CONFIGURATION -----
+(def service-port 9090)
 (def image-path "/images")
-(def image-sizes {:small {}
-                  :med   {}
-                  :large {}})
+(def image-sizes {:small {:px 75  :quality 0.7}
+                  :med   {:px 150 :quality 0.7}
+                  :large {:px 300 :quality 0.8}})
 
 ;; Note: Consider using a nano-id (https://github.com/zelark/nano-id).
 ;; for now sticking with regular uuid per spec.
 (defn uuid [] (str (java.util.UUID/randomUUID)))
 
+;; Image store.
 (defonce images
   (atom {}))
+#_ (reset! images {})
 
 ;; Note: If behind reverse proxy, may need to get proper host and
 ;; protocol from request headers.  e.g., X-Forwarded-Proto or similar.
+;; Alternatively, the proxy could rewrite the urls.
 (defn image-url
   "Returns the full URL of an image."
   [req id size]
@@ -61,6 +75,29 @@
                                       {}
                                       (keys @images))})}))
 
+(defn resize
+  "Resizes an image to the specified size maintaining the aspect ratio."
+  ^BufferedImage [^BufferedImage image size]
+  (Scalr/resize image
+                org.imgscalr.Scalr$Method/BALANCED
+                size
+                nil))
+
+(defn remove-alpha
+  "Returns a copy of img with the alpha channel removed."
+  [img]
+  (let [width (.getWidth img)
+        height (.getHeight img)
+        target (BufferedImage. width
+                               height
+                               BufferedImage/TYPE_INT_RGB)]
+    (doto (.createGraphics target)
+      (.setColor Color/WHITE) ; TODO: customize background?
+      (.fillRect 0 0 width height)
+      (.drawImage img 0 0 nil)
+      .dispose)
+    target))
+
 (defn process-upload
   [req]
   #_ (debug req)
@@ -69,16 +106,79 @@
   ;; TODO: validate request
   (let [{:keys [content-type bytes]} (get-in req [:multipart-params "image"])
         id                           (uuid)]
-    ;; TODO: process images
-    (swap! images assoc id {:orig [content-type bytes]})
+    ;; TODO: ? does this handle png?
+    ;; TODO: ? can the same base image be used to create all versions?
+    (let [orig (-> bytes input-stream img/load-image)
+
+          ;; Process source image into target sizes.
+          imgs (reduce (fn [m [k {:keys [px quality]}]]
+                         (let [os (java.io.ByteArrayOutputStream.)]
+                           (-> orig
+                               remove-alpha ; returns a copy w/ no alpha channel
+                               ;;(img/resize 100)
+                               (resize px)
+                               (img/write os "jpg" :quality quality))
+                           (assoc m k ["image/jpeg" (.toByteArray os)])))
+                       {}
+                       image-sizes)]
+      (swap! images assoc id imgs)
+      #_ (img/show orig)
+      #_ (prn "hasAlpha" (-> orig .getColorModel .hasAlpha))
+      #_ (img/write (remove-alpha orig) "foo.jpg" "jpg" :quality 0.7)
+      #_ (img/write orig "foo.png" "png" :quality 0.7)
+      )
     (list-images req id)))
 
+;; Regex to parse image url into uuid and size.
+(def image-path-re
+  (re-pattern (str "([0-9a-f\\-]+)-("
+                   (->> image-sizes
+                        keys
+                        (map name)
+                        (str/join "|"))
+                   ").jpg")))
+
+(defn image-path->map
+  "Takes an image path and returns a map with image :id and :size."
+  [path]
+  (when-let [[_ id size] (re-matches image-path-re path)]
+    {:id id
+     :size (keyword size)}))
+#_ (image-path->map "cc572b5b-995b-4a5b-9ba2-b2d444c06638-small.jpg")
+#_ (image-path->map "cc572b5b-995b-4a5b-9ba2-b2d444c06638-huge.jpg")
+
+(defn serve-image
+  "Serves an image. The request path must have a valid uuid and size."
+  [req]
+  (when-let [{:keys [id size]} (image-path->map (get-in req [:params :image]))]
+    (when-let [[content-type bytes] (get-in @images [id size])]
+      {:headers {"Content-type" content-type}
+       :status  200
+       :body    bytes})))
+
+(defn gallery
+  "Minimal gallery page to display all images."
+  [req]
+  {:headers {#_#_"Content-type" "application/json"}
+   :status  200
+   :body    (html [:div
+                   [:h1 "gallery"]
+                   (map (fn [id]
+                          [:div
+                           id
+                           (map (fn [[_k url]]
+                                  (h/image url))
+                                (image-urls req id))])
+                        (keys @images))])})
+
 (defroutes app
-  (GET  "/"       [] "Image server home page")
-  (GET image-path [] list-images)
-  (POST "/upload" [] (-> process-upload
-                         ;; Store uploads in memory instead of temp file per requirement.
-                         (wrap-multipart-params {:store (byte-array-store)})))
+  (GET  "/"              [] "Image server home page")
+  (GET  "/images/:image" [] serve-image)
+  (GET  image-path       [] list-images)
+  (GET  "/gallery"       [] gallery)
+  (POST "/upload"        [] (-> process-upload
+                                ;; Store uploads in memory instead of temp file per requirement.
+                                (wrap-multipart-params {:store (byte-array-store)})))
   (not-found "<h1>Page not found.</h1>"))
 
 (def reloadable-app
@@ -87,6 +187,6 @@
 (defn -main
   [& _args]
   (info "Starting server")
-  (run-jetty reloadable-app {:port  9090
-                  :join? false}))
+  (run-jetty reloadable-app {:port service-port
+                             :join? false}))
 #_ (-main)
